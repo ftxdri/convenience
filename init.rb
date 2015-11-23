b96fc8b72c486d7b50c2752b59a03a29a4325421
Sergio <sergiomattei@outlook.com>
2015-11-23 18:57:57 +0000

    Created init.rb  - This scritp creates aliases and configures .bashrc

---
+++ init.rb
@@ -0,0 +1,16 @@
+# .bashrc additions
+
+# Path to your bashrc
+bashrc = '/home/ubuntu/workspace/bashrc.txt';
+
+# Aliases desired.
+aliases = [
+    'alias server_init="rails server -p  $PORT -b $IP"',
+    'alias routes="c9 config/routes.rb'
+];
+
+f = File.open(bashrc, 'a')
+
+aliases.each do |a|
+  f.puts a
+end
